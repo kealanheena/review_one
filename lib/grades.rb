d8f5da0290b89ae34ed5be92a4419798fa0594b8
kealanheena <kealanheena@gmail.com>
2020-03-19 16:32:42 +0000

    Chore: Update display to show the correct amount of amber grades

--- lib/grades.rb
+++ lib/grades.rb
@@ -1,7 +1,18 @@
 class Grades
   def display(grades)
-    green_amount = grades.split(',').count('green')
+    high_grades = number_of_grades(grades, 'green')
+    medium_grades = number_of_grades(grades, 'amber')
 
-    "Green: #{green_amount} Amber: 0 Red: 0\n#{grades}"
+    "Green: #{high_grades} Amber: #{medium_grades} Red: 0\n#{grades}"
   end
+
+  private
+
+  def number_of_grades(grades, grade_level)
+    grades.split(',').count(grade_level)
+  end
+
+  # def organise_grades(grades)
+  #   grades.split(',').join("\n")
+  # end
 end
--- spec/grades_spec.rb
+++ spec/grades_spec.rb
@@ -7,9 +7,21 @@ describe Grades do
       expect(@grades.display("")).to eq("Green: 0 Amber: 0 Red: 0\n")
     end
 
-    it 'should take a string and diplay the amount of each grade' do
-      @grades = Grades.new()
-      expect(@grades.display("green")).to eq("Green: 1 Amber: 0 Red: 0\ngreen")
+    context 'when a single grade is entered' do
+      it 'should diplay the grade after all the amounts & show the correct amount of grades' do
+        @grades = Grades.new()
+        expect(@grades.display("green")).to eq("Green: 1 Amber: 0 Red: 0\ngreen")
+      end
+
+      it 'should diplay the grade after all the amounts & show the correct amount of grades' do
+        @grades = Grades.new()
+        expect(@grades.display("amber")).to eq("Green: 0 Amber: 1 Red: 0\namber")
+      end
     end
+
+    # it 'should diplay the "green" grades before the "amber" grades' do
+    #   @grades = Grades.new()
+    #   expect(@grades.display("green,amber")).to eq("Green: 1 Amber: 1 Red: 0\ngreen\namber")
+    # end
   end
 end
